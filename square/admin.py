f60f0f929c1fc5e4ae5ebf5860a39f2e4a4758fe
Brian Peterson <bepetersn@gmail.com>
2013-08-07 19:46:11 -0500

    admin add_event page now lets you add sessions directly

--- square/admin.py
+++ square/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from square.models import Event, EventLocation, Session, Volunteer
 
-admin.site.register(Session)
-admin.site.register(Event)
+class SessionInline(admin.TabularInline):
+    model = Session
+    extra = 1
+
+class EventAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Event Info', {'fields' : ['type', 'event_location']}),
+    	('Time Info',  {'fields' : ['date', 'start', 'end']}),
+        ('Other',      {'fields' : ['notes'], 'classes': ['collapse']}),
+    ]
+    inlines = [SessionInline]
+
+admin.site.register(Event, EventAdmin)
 admin.site.register(EventLocation)
 admin.site.register(Volunteer)

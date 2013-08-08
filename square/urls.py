8925c5330a6178d8a86735f31014428ce6d1210b
Brian Peterson <bepetersn@gmail.com>
2013-08-08 02:09:41 -0500

    removed unnecessary URLS; left volunteer-search, which I will build with bootstrap, and volunteer-browse, which is for showcasing that townsquare works right now.

--- square/urls.py
+++ square/urls.py
@@ -2,10 +2,6 @@ from django.conf.urls import patterns, url
 from square import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
     url(r'^volunteers/search$', views.search_volunteers),
     url(r'^volunteers/browse$', views.browse_volunteers),
-    url(r'^volunteers/signup$', views.signup_volunteers),
-    url(r'^events/create$', views.create_event),
-    url(r'^events/search$', views.search_events)
 )
--- square/views.py
+++ square/views.py
@@ -1,10 +1,6 @@
 from django.http import HttpResponse
 from square.models import Volunteer
 
-def index(request):
-    output = "Some cool stuff."
-    return HttpResponse(output)
-
 def browse_volunteers(request):
     output = "Here are all the volunteers. </br></br>"
     volunteers = Volunteer.objects.all()
@@ -15,12 +11,3 @@ def browse_volunteers(request):
 def search_volunteers(request):
     output = "Which volunteer do you want to view?"
     return HttpResponse(output)
-
-def signup_volunteers(request):
-    return HttpResponse("Start signing up volunteers here")
-
-def create_event(request):
-    return HttpResponse("Create event here")
-
-def search_events(request):
-    return HttpResponse("Which event do you want to view?")

5cc88630b235437151e06c2f037c5c0aa0687ed3
Matt <miserypanda@gravework.com>
2013-08-24 14:28:11 -0500

    Added Bootstrap. Rendering the browse_volunteers view through a template. Added a bit about template directories to the readme and added a requirements file.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ townsquare2
 ===========
 
 Python-Django port of the Townsquare project found at https://github.com/sc3/townsquare.
+
+
+Must configure django-townsquare/settings.py's TEMPLATE_DIRS to 
+the absolute path for your templates directory.
--- square/views.py
+++ square/views.py
@@ -1,12 +1,26 @@
 from django.http import HttpResponse
 from square.models import Volunteer
+from django.template import Context, Template, loader, RequestContext
+from django.shortcuts import render
+
 
 def browse_volunteers(request):
-    output = "Here are all the volunteers. </br></br>"
+    
+    #Assigning information of all volunteers to a variable
     volunteers = Volunteer.objects.all()
-    for volunteer in volunteers:
-        output += "%s, hours worked: %s </br>" % (volunteer.name, volunteer.calculate_hours())
-    return HttpResponse(output)
+    
+    #Loading template in "t" and assigning variable to context in "c"
+    t = loader.get_template('users/volunteer_browse.html')
+    c = RequestContext(request, {'volunteers':volunteers,})
+    
+    #Compiling template and rendering out the context information
+    r = t.render(c)
+    
+    #Returning the compiled template with rendered info to template
+    return HttpResponse(r)
+    
+    
+browse_volunteers.alters_data=False
 
 def search_volunteers(request):
     output = "Which volunteer do you want to view?"

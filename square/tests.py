cad5257aa3531533d70f46aed0b3b01d3d1dc10e
Brian Peterson <bepetersn@gmail.com>
2013-11-21 07:53:07 -0600

    add two succeeding tests and one failing

--- square/tests.py
+++ square/tests.py
@@ -1,16 +1,25 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.test.client import Client
+from square.models import Volunteer
+from django.contrib.auth.models import User
 
+class Test_Volunteer_URLs(TestCase):
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+	def test_volunteer_login_up(self):
+		self.client = Client()
+		response = self.client.get('/townsquare/login')
+		self.assertEqual(response.status_code, 200)
+
+	def test_volunteer_add_redirects_if_not_logged_in(self):
+		response = self.client.get('/townsquare/volunteer/add', follow=True)
+		self.assertEqual(response.request['PATH_INFO'], '/townsquare/login')
+
+	def test_volunteer_add_fails_if_user_not_staff(self):
+		u1 = User.objects.create_user(first_name="alan", last_name="watts", 
+										username="awatts", password="secret")
+		v1 = Volunteer(user=u1)
+		u1.save()
+		v1.save()
+		self.client.login(username='awatts', password="secret")
+		response = self.client.get('/townsquare/volunteer/add', follow=True)
+		self.assertEqual(response.status_code, 401)

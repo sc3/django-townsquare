ae102d5fbb161f4e8700c56dca3f5bbf14e4a3a1
Brian Peterson <bepetersn@gmail.com>
2013-11-21 05:33:36 -0600

    cleaned up urlconf

--- square/urls.py
+++ square/urls.py
@@ -2,26 +2,16 @@ from django.conf.urls import patterns, url
 from square import views
 
 urlpatterns = patterns('',
-    url(r'^volunteers/home$', views.home),
-    
+
     url(r'^about$', views.about),
-    
     url(r'^login$', views.t2login),
-    
-    #Processing for t2login
-    url(r'^login2$', views.t2login2),
-    
-    url(r'^volunteer/add$', views.t2signup),
-    
-    #Processing for t2signup
-    url(r'^signup2$', views.t2signup2),
-    
     url(r'^logout$', views.t2logout),
-    
-    url(r'^event/add$', views.add_event),
-    url(r'^event2/add$', views.t2_addevent),
-    
-    
-    
+    url(r'^volunteers/homes$', views.home),
+    url(r'^volunteer/add$', views.t2signup),
+    url(r'^event/add$', view.add_event)
+
+    # Processing views
+    url(r'^login2$', views.t2login2),
+    url(r'^signup2$', views.t2signup2),
     
 )

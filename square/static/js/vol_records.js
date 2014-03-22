83321f9961a70521cbd9cd1044f0c9d910968653
Matt <miserypanda@gravework.com>
2014-03-22 16:19:09 -0500

    Broke the volunteer home. Still need to figure out typeahead's filters for nested JSON objects.

---
+++ square/static/js/vol_records.js
@@ -0,0 +1,48 @@
+//Attempt to create a typeahead field with prefetched JSON data
+
+
+var volrecords = new Bloodhound(
+{
+
+	datumTokenizer: Bloodhound.tokenizers.obj.whitespace('dates'),
+	queryTokenizer: Bloodhound.tokenizers.whitespace,
+	//limit: 100,
+	
+	prefetch:
+	{
+
+		url: '../api/v1/volunteers/?format=json',
+
+		//Necessary when dealing with arrays of strings or nested JSON objects
+		/*filter: function(data)
+		{
+
+			return $.map(list, function(signup_date)
+			{
+				return {dates:signup_date};
+			});
+
+		}*/
+
+	}
+
+});
+
+
+
+//Starting processing/loading of "local" and "prefetch"
+volrecords.initialize();
+
+
+$('#volrec .typeahead').typeahead(null,
+{
+
+	name: 'vol-records',
+	displayKey: 'dates',
+
+
+	//'ttAdapter' wraps the suggestion engine in an apdapter compatible with the ta plugin
+	source: volrecords.ttAdapter()
+
+});
+

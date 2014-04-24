0c4949c02ff853bb2f07b1506a300ec1a5a1b044
Brian Peterson <bepetersn@gmail.com>
2014-04-24 00:19:45 -0500

    fix login refactoring bugs; update UI

--- square/views.py
+++ square/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from square.forms import EventForm, VolunteerForm, LoginForm
 from square.models import Volunteer, Event
@@ -24,12 +24,12 @@ def t2login(request):
         form = LoginForm(request.POST)
         
         if form.is_valid():
-            
-            succeeded = process_valid_login(form, request)
+
+            succeeded = process_valid_login_post(request, form)
             if succeeded:
-                HttpResponseRedirect('/townsquare/volunteer/browse')
+                return HttpResponseRedirect('/townsquare/volunteer/home')
             else:
-                HttpResponseRedirect('/townsquare/login')
+                return HttpResponseRedirect('/townsquare/login')
     
     # render result of an invalid POST or a GET request
     return render(request, 'users/login.html', 
